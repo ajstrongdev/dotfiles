fe84c4d4c9f5f22896c3015ac3f059632c00722c
ajstrongdev <ajstrongdev@proton.me>
2025-06-12 00:29:33 +0100

    Screenshot tweaks

--- rofi/screenshot.sh
+++ rofi/screenshot.sh
@@ -7,13 +7,13 @@ options=(
 chosen=$(printf '%s\n' "${options[@]}" | rofi -dmenu -p "Take Screenshot")
 case "$chosen" in
    "Select Area (Copy)")
-	   grimshot --wait 1 copy area
+	   grimshot --wait 1 copy area && notify-send "Screenshot copied to clipboard!"
   	;;
   "Active Window (Copy)")
-    grimshot --wait 1 copy active
+    grimshot --wait 1 copy active && notify-send "Screenshot copied to clipboard!"
     ;;
 
   "Screen (Copy)")
-    grimshot --wait 1 copy screen && notify-send "Screenshot taken"
+    grimshot --wait 1 copy screen && notify-send "Screenshot copied to clipboard!"
     ;;
 esac
