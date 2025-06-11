ab0f1378e6f7b5935f94443e8224f675a4f4068e
ajstrongdev <ajstrongdev@proton.me>
2025-06-11 23:46:49 +0100

    Selection screenshot

--- rofi/screenshot.sh
+++ rofi/screenshot.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 options=(
+  "Select Area (Copy)"
   "Active Window (Copy)"
   "Screen (Copy)"
 )
 chosen=$(printf '%s\n' "${options[@]}" | rofi -dmenu -p "Take Screenshot")
 case "$chosen" in
+   "Select Area (Copy)")
+	   grimshot --wait 1 copy area
+  	;;
   "Active Window (Copy)")
     grimshot --wait 1 copy active
     ;;
